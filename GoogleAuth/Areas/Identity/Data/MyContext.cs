e3e028533f26defb4e300188262390a1449bba01
Hashim Al-Mansouri <HashimAlmansouri@outlook.com>
2023-02-18 10:42:17 +0300

    Add project files.

---
+++ GoogleAuth/Areas/Identity/Data/MyContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace GoogleAuth.Data;
+
+public class MyContext : IdentityDbContext<IdentityUser>
+{
+    public MyContext(DbContextOptions<MyContext> options)
+        : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+        // Customize the ASP.NET Identity model and override the defaults if needed.
+        // For example, you can rename the ASP.NET Identity table names and more.
+        // Add your customizations after calling base.OnModelCreating(builder);
+    }
+}
